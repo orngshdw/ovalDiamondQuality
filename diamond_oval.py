fc515124afe2e0558653b851d6b8f3f68ef8bf11
Kenny <kenny.hoang87@gmail.com>
2018-05-15 21:44:29 -0400

    Updated to be more Pythonic.

--- diamond_oval.py
+++ diamond_oval.py
@@ -1,5 +1,5 @@
-"""Evaluates oval diamonds. User enters the Table %, Depth %, length, and width numbers
-and gets diamond quality based on https://www.diamonds.pro/education/oval-cut/
+"""User enters the Table %, Depth %, length, and width numbers
+to get diamond quality based on https://www.diamonds.pro/education/oval-cut/
 Creator: Kenny Hoang
 MIT License
 """
@@ -9,7 +9,7 @@ import math
 """Table% are (from Excellent to Poor): 
     53-63, 52 or 64-65, 51 or 66-68, 50 or 69-70, <50 or >70
     """
-tablePercRef = {
+table_reference = {
     "Excellent": [[53.0, 10.0]],
     "Very Good": [[52.0,  1.0], [63.0,  2.0]],
          "Good": [[51.0,  1.0], [65.0,  3.0]],
@@ -20,7 +20,7 @@ tablePercRef = {
 """Depth %	are (from Excellent to Poor):
     58-62, 56-57.9 or 62.1-66, 53-55.9 or 66.1-71, 50-52.9 or 71.1-74, <50 or >74
     """
-depthPercRef = {
+depth_reference = {
     "Excellent": [[58.0, 4.0]],
     "Very Good": [[56.0, 2.0], [62.0, 4.0]],
          "Good": [[53.0, 3.0], [66.0, 5.0]],
@@ -28,10 +28,10 @@ depthPercRef = {
          "Poor": [[0.0, 50.0], [74.0, 26.0]]
 }
 
-"""Length Width Ratios	are (from Excellent to Poor):
+"""Length/Width Ratios	are (from Excellent to Poor):
     1.35-1.50, 1.30-1.34 or 1.51-1.55, 1.25-1.29 or 1.56-1.60, 1.20-1.24 or 1.61-1.65, <1.20 or >1.65
     """
-lenWidRatioRef = {
+length_width_reference = {
     "Excellent": [[1.35, 0.15]],
     "Very Good": [[1.30, 0.05], [1.50, 0.05]],
          "Good": [[1.25, 0.05], [1.55, 0.05]],
@@ -39,41 +39,36 @@ lenWidRatioRef = {
          "Poor": [[0.00, 1.20], [1.65, 100.0]]
 }
 
-questions = ["What is the Table Percentage? ", "What is the Depth Percentage? ", \
-             "What is the length of diamond? ", "What is the width of diamond? "]
-strDiamondProp = ["Table %", "Depth % ", "Length/Width ratio"]
-diamondProp = [tablePercRef, depthPercRef, lenWidRatioRef]
-buyProp = [0.0, 0.0, 0.0, 0.0]
+questions = ["What is the Table Percentage? ", "What is the Depth Percentage? ", "What is the length of diamond? ", "What is the width of diamond? "]
+str_diamond_prop = ["Table %", "Depth % ", "Length/Width ratio"]
+diamond_prop = [table_reference, depth_reference, length_width_reference]
+buy_prop = [0.0, 0.0, 0.0, 0.0]
 
-valueAssigned, q = 0, 0
+value_assigned, q = False, 0
 
-while valueAssigned == 0:
+while value_assigned is False:
     while q < 4:
         try:
-            buyProp[q] = float(input(questions[q]))
-            assert math.sqrt(buyProp[q]) < 10.0
+            buy_prop[q] = float(input(questions[q]))
+            assert math.sqrt(buy_prop[q]) < 10.0
             q += 1
         except(AssertionError, ValueError):
             print("Please enter a number between 0 and 100")
 
-    buyProp[2] = buyProp[2]/buyProp[3]
-    valueAssigned = 1
+    buy_prop[2] = buy_prop[2]/buy_prop[3]
+    value_assigned = True
 
 print("\nScale: Excellent, Very Good, Good, Fair, Poor")
 
 for p in range(3):
-    match = 0
-    for quality in diamondProp[p]:
-        for ranges in diamondProp[p][quality]:
-            if buyProp[p] >= ranges[0] and buyProp[p] <= (ranges[0]+ranges[1]):
-                match = 1
-                print("{}, {}\n{}".format(strDiamondProp[p], buyProp[p], quality))
-                break
+    match = False
+    for quality, ranges in diamond_prop[p].items():
+        for value in ranges:
+            if buy_prop[p] >= value[0] and buy_prop[p] <= (value[0]+value[1]):
+                print("{} is {}, {}".format(str_diamond_prop[p], quality, buy_prop[p]))
+                match = True
 
-            if match == 1:
-                break
-
-        if match == 1:
+        if match is True:
             break
 
 # Formatting purposes at the end of output
